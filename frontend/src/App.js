3a1d45b61adb189a41988ea88cac2c888e0f2b76
jedatu <jedatu@gmail.com>
2017-10-08 13:30:39 -0400

    Add faces

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+import face1 from './1.svg';
+import face2 from './2.svg';
+import face3 from './3.svg';
+import face4 from './4.svg';
+import face5 from './5.svg';
+
 import axios from 'axios';
+import auth0 from 'auth0-js';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +17,14 @@ class App extends Component {
   constructor(props, context) {
     super(props, context)
     this.state = {
-      loading: false
+      loading: false,
+      emojis: [
+        {id: 1, name: "joy", face: face1 },
+        {id: 2, name: "happy", face: face2 },
+        {id: 3, name: "satisfied", face: face3 },
+        {id: 4, name: "stressed", face: face4 },
+        {id: 5, name: "angry", face: face5 }
+      ]
     }
   }
 
@@ -17,12 +32,21 @@ class App extends Component {
     alert('login');
   }
 
-  doAjax = () => {
+  doAjax = (id) => {
     axios.post('https://x5p9i5jbd5.execute-api.us-east-1.amazonaws.com/dev/humoji', {
-      mood: 3
+      mood: id
     }).then(response => {
       console.log(response.data);
       this.setState(response.data);
+    });
+  }
+
+  renderEmojis() {
+    return this.state.emojis.map((emoji, n) => {
+      return (<img key={emoji.id}
+        style={{width:'10%', padding:'10px'}}
+        onClick={this.doAjax.bind(this, emoji.id)}
+        alt={emoji.name} src={emoji.face} />);
     });
   }
 
@@ -34,10 +58,10 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">humoji</h1>
+          <button onClick={this.doLogin}>Login</button>
         </header>
         <p className="App-intro">
-          <button onClick={this.doLogin}>Login</button>
-          <button onClick={this.doAjax}>Ajax</button>
+          {this.renderEmojis()}
         </p>
         <p>
           {mood}
